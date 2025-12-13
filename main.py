dc73a1feb755e6e5e94e3dc3a11fa2c980ce2662
giriYT26 <srikalgiri@gmail.com>
2025-12-13 13:04:45 +0530

    feat:add player animation

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from tkinter.messagebox import showerror,showwarning
 from os.path import join,dirname,isfile
 from random import choice
 #global GRAVITY,PLAYER_MOVEMENT
-print("game starts")
+#print("game starts")
 WIDTH = 576 
 HEIGHT = 1024
 FPS = 60 #130
@@ -12,15 +12,29 @@ PLY_MOV = 0
 CUR_DIR = dirname(__file__)
 BG_IMG = join(CUR_DIR,"sprites","background-day.png")
 FLOOR_IMG = join(CUR_DIR,"sprites","base.png")
-PLAYER_IMG = join(CUR_DIR,"sprites","redbird-midflap.png")
 OBS_IMG = join(CUR_DIR,"sprites","pipe-green.png")
+PLAYER1_IMG = join(CUR_DIR,"sprites","redbird-downflap.png")
+PLAYER2_IMG = join(CUR_DIR,"sprites","redbird-midflap.png")
+PLAYER3_IMG = join(CUR_DIR,"sprites","redbird-upflap.png")
 
 class Player:
     def __init__(self):
         self.bird_movement = PLY_MOV
-        self.bird_surface = pygame.image.load(PLAYER_IMG).convert_alpha()
-        self.bird_surface = pygame.transform.scale2x(self.bird_surface)
+        self.bird_idx = 0
+        #self.bird_surface = pygame.image.load(PLAYER_IMG).convert_alpha()
+        self.sprite_load() # Loads the sprites before the game starts
+        self.animation()
         self.bird_rect = self.bird_surface.get_rect(center=(100,600)) #100,512
+        self.bird_flaps = pygame.USEREVENT + 1
+        pygame.time.set_timer(self.bird_flaps,200) #Created a user event that triggers for every 200ms
+    def sprite_load(self):
+        bird_downflap = pygame.image.load(PLAYER1_IMG).convert_alpha()
+        bird_midflap = pygame.image.load(PLAYER2_IMG).convert_alpha()
+        bird_upflap = pygame.image.load(PLAYER3_IMG).convert_alpha()
+        self.bird_frame = [bird_downflap,bird_midflap,bird_upflap]
+    def animation(self):
+        self.bird_surface = self.bird_frame[self.bird_idx]
+        self.bird_surface = pygame.transform.scale2x(self.bird_surface)
     def update(self):
         self.bird_movement += GRAVITY
         self.bird_rect.centery += self.bird_movement
@@ -53,7 +67,7 @@ class Pipes:
                 print("game_over") 
                 return False
         return True
-    def move_pipes(self):
+    def move_pipes(self)->list:
         """Move the pipes to left"""
         for pipe in self.pipe_lst:
             pipe.centerx -= 5
@@ -116,6 +130,13 @@ class Game:
                 if event.type == self.pipe.spawnpipe :
                     self.pipe.pipe_lst.extend(self.pipe.create_pipe())
                     #print(self.pipe.pipe_lst)
+                if event.type == self.player.bird_flaps:
+                    if self.player.bird_idx < 2:
+                        self.player.bird_idx +=1
+                        #print(self.player.bird_idx)
+                    else:
+                        self.player.bird_idx = 0
+                    self.player.animation()
 
             #background 
             self.bg.update()
