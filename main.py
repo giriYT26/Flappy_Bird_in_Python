32c052359c9b9027dfba4b26dce163d749f6b7e2
giriYT26 <srikalgiri@gmail.com>
2025-12-13 11:35:50 +0530

    feat:add many stuff (i forgot to push this to my repo)

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ from random import choice
 print("game starts")
 WIDTH = 576 
 HEIGHT = 1024
-FPS = 130
+FPS = 60 #130
 GRAVITY = 0.24
+PLY_MOV = 0
 CUR_DIR = dirname(__file__)
 BG_IMG = join(CUR_DIR,"sprites","background-day.png")
 FLOOR_IMG = join(CUR_DIR,"sprites","base.png")
@@ -16,15 +17,20 @@ OBS_IMG = join(CUR_DIR,"sprites","pipe-green.png")
 
 class Player:
     def __init__(self):
-        self.bird_movement = 0
-        self.bird_surface = pygame.image.load(PLAYER_IMG).convert()
+        self.bird_movement = PLY_MOV
+        self.bird_surface = pygame.image.load(PLAYER_IMG).convert_alpha()
         self.bird_surface = pygame.transform.scale2x(self.bird_surface)
-        self.bird_rect = self.bird_surface.get_rect(center=(100,HEIGHT//2))
+        self.bird_rect = self.bird_surface.get_rect(center=(100,600)) #100,512
     def update(self):
         self.bird_movement += GRAVITY
         self.bird_rect.centery += self.bird_movement
+    def ply_rotate(self):
+        self.Rotated_bird_surface = pygame.transform.rotozoom(self.bird_surface,-self.bird_movement*3,1)
+        return self.Rotated_bird_surface
     def draw(self,surface):
-        surface.blit(self.bird_surface,self.bird_rect)
+        rotated_bird_surface = self.ply_rotate()
+        surface.blit(rotated_bird_surface,self.bird_rect)
+    
 class Pipes:
     def __init__(self):
         self.pipe_surface = pygame.image.load(OBS_IMG).convert()
@@ -39,7 +45,14 @@ class Pipes:
         self.bottom_pipe = self.pipe_surface.get_rect(midtop=(700,random_pipe_height)) #700,512
         self.top_pipe = self.pipe_surface.get_rect(midbottom=(700,random_pipe_height-300))
         return self.bottom_pipe,self.top_pipe
-    
+    def check_collision(self,bird):
+        for pipe in self.pipe_lst:
+            if bird.colliderect(pipe):
+                return False
+            elif bird.top <= -100 or bird.bottom >= 790:
+                print("game_over") 
+                return False
+        return True
     def move_pipes(self):
         """Move the pipes to left"""
         for pipe in self.pipe_lst:
@@ -48,7 +61,7 @@ class Pipes:
     
     def draw(self,surface):
         for pipe in self.pipe_lst:
-            if pipe.bottom >= 1028:
+            if pipe.bottom >= 900:#1028
                 surface.blit(self.pipe_surface,pipe)
             else:
                 filp_pipe = pygame.transform.flip(self.pipe_surface,False,True)
@@ -72,10 +85,12 @@ class Background:
     def draw_floor(self,surface):
         surface.blit(self.floor,(self.floor_x_pos,790))
         surface.blit(self.floor,(self.floor_x_pos+self.floor.get_width(),790)) #0,790 -> correct pos of the base
+
 class Game:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((576,900))
+        self.game_active = True
         self.clock = pygame.time.Clock()
         self.bg = Background()
         self.player = Player()
@@ -87,23 +102,39 @@ class Game:
                     pygame.quit()
                     os.kill(os.getpid(),signal.SIGTERM)
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_SPACE or event.key == pygame.K_UP:
+                    if event.key == pygame.K_SPACE and self.game_active == True:
                         self.player.bird_movement = 0
-                        self.player.bird_movement -=12
+                        #for the default game 9 is good for 60fps
+                        self.player.bird_movement -= 9 #12 when hard or 10 it's ok to handle it and if the game what to be easy set it to 5-8
+                    if event.key == pygame.K_SPACE and self.game_active == False:
+                        self.game_active = True
+                        self.pipe.pipe_lst.clear()
+                        self.player.bird_rect.center = (100,450) #100,512
+                        self.player.bird_movement = PLY_MOV
+                        #self.player.
+
                 if event.type == self.pipe.spawnpipe :
                     self.pipe.pipe_lst.extend(self.pipe.create_pipe())
                     #print(self.pipe.pipe_lst)
+
             #background 
             self.bg.update()
             self.bg.draw_background(self.screen)
-            #pipes
-            self.pipe.pipe_lst = self.pipe.move_pipes()
-            self.pipe.draw(self.screen )
+
+            #this below line will work only if the game is active
+            if self.game_active:
+                #pipes
+                self.pipe.pipe_lst = self.pipe.move_pipes()
+                self.pipe.draw(self.screen)
+                #player
+                self.player.update()
+                self.player.draw(self.screen)
+                #Checking for collision
+                self.game_active= self.pipe.check_collision(self.player.bird_rect)
+
             #floor
             self.bg.draw_floor(self.screen)
-            #player
-            self.player.update()
-            self.player.draw(self.screen)
+
             pygame.display.update()
             self.clock.tick(FPS)
 
